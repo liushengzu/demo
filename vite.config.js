55764e370d687d777822fdc3d6c5dfe656267ff8
liushengzu <liushengzu88@gmail.com>
2025-07-20 10:17:52 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import { functionsScanner, copyFile } from "wpsjs/vite_plugins"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    plugins: [
+        copyFile({ src: 'manifest.xml', dest: 'manifest.xml', }),
+        copyFile({ src: 'js', dest: 'js', }),
+        copyFile({ src: 'images', dest: 'images', }),
+        copyFile({ src: 'ui', dest: 'ui', }),
+        copyFile({ src: 'main.js', dest: 'main.js', }),
+        functionsScanner({
+            inputJsPath: 'js/functions.js',
+            outputJsonPath: 'functions.json',
+            namespace: 'HelloEt',
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: '0.0.0.0'
+    }
+})
